6e5b4a8c8749d509d87c3fb1f0415199cb9831a0
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-02-13 17:31:38 +0700

    start save step onboard

--- app/src/main/java/com/example/shop/domain/OnboardingViewModel.kt
+++ app/src/main/java/com/example/shop/domain/OnboardingViewModel.kt
@@ -1,15 +1,49 @@
 package com.example.shop.domain
 
 import android.app.Application
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.shop.data.dataStore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
-class OnboardingViewModel(application: Application) :AndroidViewModel(application) {
+class OnboardingViewModel(application: Application) : AndroidViewModel(application) {
 
     private val dataStore = application.dataStore
 
+//    fun saveName(name: String) {
+//        viewModelScope.launch {
+//            saveName(name)
+//        }
+//    }
 
-    fun saveStep(){
+    fun getName(): MutableStateFlow<Int> {
+        val liveData = MutableStateFlow<Int>(1)
+        viewModelScope.launch {
+            liveData.postValue(getName())
 
+        }
+        return liveData
+    }
+
+    suspend fun saveName(name: String) {
+        val nameKey = stringPreferencesKey("name")
+        dataStore.edit { preferences ->
+            preferences[nameKey] = name
+        }
+    }
+
+    suspend fun getName(): String? {
+        val nameKey = stringPreferencesKey("name")
+        return dataStore.data
+            .map { preferences ->
+                preferences[nameKey] ?: "Default Name"
+            }
+            .first() // Получаем первое значение из потока
     }
 }
