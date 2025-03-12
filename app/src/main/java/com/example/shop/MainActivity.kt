d6de5160aa3de18ec7b4ddc76ef039ec46cc6b16
DRANIK1211 <100068733+DRANIK1211@users.noreply.github.com>
2025-03-12 22:47:25 +0700

    Update MainActivity.kt
    asdafsdsfgadgsf

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -17,6 +17,67 @@ import com.example.shop.home.HomeScreen
 import com.example.shop.onboard.Onboard
 import com.example.shop.ui.theme.ShopTheme
 
+@Composable 
+fun BrightnessControl() {
+    val context = LocalContext.current
+    var isPopupVisible by remember { mutableStateOf(false) }
+    val originalBrightness = remember { 
+        Settings.System.getInt(
+            context.contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS,
+            255
+        )
+    }
+    val main = Settings.System.getInt(
+            context.contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS,
+            255
+        )
+
+    Button(
+        onClick = { 
+            if (Settings.System.canWrite(context)) {
+                Settings.System.putInt( 
+        context.contentResolver,
+        Settings.System.SCREEN_BRIGHTNESS,
+        204
+    )
+                isPopupVisible = true
+            } 
+        }
+    ){
+        Text("Изменить яркость") 
+    } 
+    
+    if (isPopupVisible) {
+        Popup(onDismissRequest = {
+            Settings.System.putInt( 
+        context.contentResolver,
+        Settings.System.SCREEN_BRIGHTNESS,
+        204
+    )
+            isPopupVisible = false 
+        }){
+            Box(
+                modifier = Modifier.fillMaxSize().clickable{
+                    isPopupVisible = false 
+            }){
+                Text("Нажмите здесь, чтобы закрыть попап",
+                    modifier = Modifier.clickable{ 
+                        Settings.System.putInt( 
+        context.contentResolver,
+        Settings.System.SCREEN_BRIGHTNESS,
+        main
+    )
+                        isPopupVisible = false
+                })
+            }
+        }
+    }
+} 
+
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
